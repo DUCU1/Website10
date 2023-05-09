fa9ae11363e5da63e66c104a81a294c7dac04840
Jasper_Marichal <jasper.marichal@student.kdg.be>
2023-05-09 16:12:24 +0200

    Fixed the loginForm.html basic version, added a JSON file to start and try modifying the loginForm.html to eventually implement it in the site

---
+++ javascript/db/users.js
@@ -0,0 +1,20 @@
+export const usersJSON = [
+    {
+        username: "Jasper",
+        email: "jasper.marichal@student.kdg.be",
+        password: "initial01",
+        role: "admin"
+    },
+    {
+        username: "Vlad",
+        email: "Vlad@student.kdg.be",
+        password: "initial01",
+        role: "admin"
+    },
+    {
+        username: "Player123",
+        email: "player@student.kdg.be",
+        password: "initial01",
+        role: "player"
+    }
+]
