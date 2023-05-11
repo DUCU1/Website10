0c9270ce329bc853014d0b9eccd63af4ff2939fe
vladbuinceanu <vladgabriel.buinceanu@student.kdg.be>
2023-05-12 01:03:45 +0200

    second section finised with model video, also responsive

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+videoControl();
+
+function videoControl(){
+    let video = document.getElementById("gameplay");
+
+    video.addEventListener("click", () => {
+        if (video.paused){
+            video.play();
+        } else{
+            video.pause();
+        }
+    });
+}
