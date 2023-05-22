b590c170a65be3bddff4a0a2ca9ffe7cea1757db
vladbuinceanu <vladgabriel.buinceanu@student.kdg.be>
2023-05-22 10:51:43 +0200

    data model

--- javascript/db/testDB.js
+++ javascript/db/testDB.js
@@ -1,16 +1,79 @@
 const gamesJson = [
   {
-      id: 1,
-      playerName: "Josefine",
-      score: 200,
-      win: true,
-      date: "2006-05-04 00:00:00"
+    id: 1,
+    playerName: "Josefine",
+    score: 64,
+    win: true,
+    date: "2006-05-04 00:00:00"
   },
   {
-      id: 2,
-      playerName: "Andrew",
-      score: 600,
-      win: false,
-      date: "2008-10-25 00:00:00"
+    id: 2,
+    playerName: "Andrew",
+    score: 64,
+    win: false,
+    date: "2008-10-25 00:00:00"
+  },
+  {
+    id: 3,
+    playerName: "Emma",
+    score: 64,
+    win: true,
+    date: "2010-07-12 00:00:00"
+  },
+  {
+    id: 4,
+    playerName: "Michael",
+    score: 64,
+    win: true,
+    date: "2012-03-18 00:00:00"
+  },
+  {
+    id: 5,
+    playerName: "Sophia",
+    score: 64,
+    win: true,
+    date: "2014-09-30 00:00:00"
+  },
+  {
+    id: 6,
+    playerName: "Ethan",
+    score: 64,
+    win: true,
+    date: "2016-12-05 00:00:00"
+  },
+  {
+    id: 7,
+    playerName: "Olivia",
+    score: 64,
+    win: false,
+    date: "2019-02-17 00:00:00"
+  },
+  {
+    id: 8,
+    playerName: "Noah",
+    score: 64,
+    win: true,
+    date: "2021-06-29 00:00:00"
+  },
+  {
+    id: 9,
+    playerName: "Ava",
+    score: 64,
+    win: true,
+    date: "2023-01-10 00:00:00"
+  },
+  {
+    id: 10,
+    playerName: "Liam",
+    score: 50,
+    win: false,
+    date: "2023-04-05 00:00:00"
+  },
+  {
+    id: 11,
+    playerName: "Isabella",
+    score: 64,
+    win: true,
+    date: "2023-05-08 00:00:00"
   }
-]
+];
