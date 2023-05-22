fb9d6e68179ad58246daf0244da60aa1453a9195
vladbuinceanu <vladgabriel.buinceanu@student.kdg.be>
2023-05-22 10:08:35 +0200

    fixed navbar

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,16 +1,17 @@
 dropDown();
 
+
 function dropDown(){
     let hamMenuIcon = document.getElementById("drop-down");
     let navBar = document.getElementById("nav-bar");
-    let navLinks = navBar.querySelectorAll("li")
-    let navT = document.querySelector("nav")
+    let navLinks = navBar.querySelectorAll("li");
+    let navT = document.querySelector("nav");
 
     hamMenuIcon.addEventListener("click", () => {
         navBar.classList.toggle("active");
         hamMenuIcon.classList.toggle("active");
         navT.classList.toggle("active");
-        exit.classList.toggle("active")
+        exit.classList.toggle("active");
         navBar.classList.toggle("active");
     });
 
